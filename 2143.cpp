971c6a866f8b0a4b0b2ae5bfc799042574c04e7f
ksww1891 <137411976+ksww1891@users.noreply.github.com>
2023-11-30 01:45:04 +0000

    두 배열의 합, binarySearch

---
+++ 2143.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int t;
+int n, m, answer;
+int a[1002], b[1002], aSum[1002], bSum[1002];
+vector<int>va;
+vector<int>vb;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+    cin >> t;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> a[i];
+        aSum[i] = aSum[i-1] + a[i];
+        for(int j = 0; j < i; j++){
+            va.push_back(aSum[i] - aSum[j]);
+        }
+    }
+    cin >> m;
+    for(int i = 1; i <= m; i++){
+        cin >> b[i];
+        bSum[i] = bSum[i-1] + b[i];
+        for(int j = 0; j < i; j++){
+            vb.push_back(bSum[i] - bSum[j]);
+        }
+    }
+    sort(va.begin(), va.end());
+    for(int i = 0; i < vb.size(); i++){
+        int findT = t-vb[i];
+        int cnt = upper_bound(va.begin(), va.end(), findT) - lower_bound(va.begin(), va.end(), findT);
+        answer += cnt;
+    }
+    cout << answer;
+}
