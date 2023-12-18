596182152d688664c437aab50dd5f92f646fa012
ksww1891 <137411976+ksww1891@users.noreply.github.com>
2023-12-18 03:01:07 +0000

    별자리 만들기, MST

--- 4386.cpp
+++ 4386.cpp
@@ -10,7 +10,7 @@ int n;
 int p[101];
 double answer;
 
-bool find (int x){
+int find (int x){
     if(p[x] == x)return x;
     else return p[x] = find(p[x]);
 }
@@ -26,23 +26,20 @@ int main(){
         star.push_back({x, y});
         p[i] = i;
     }
-    for(int i = 0 ; i < star.size()-1; i++){
-        for(int j = i+1; j < star.size(); j++){
+    for(int i = 0; i < n-1; i++){
+        for(int j = i+1; j < n; j++){
             double cost = sqrt((star[i].first - star[j].first)*(star[i].first - star[j].first)
                         + (star[i].second - star[j].second)*(star[i].second - star[j].second));
             edge.push_back({cost, {i, j}});
         }
     }
     sort(edge.begin(), edge.end());
-    int cnt = 0;
     for(int i = 0; i < edge.size(); i++){
         int star1 = find(edge[i].second.first);
         int star2 = find(edge[i].second.second);
         if(star1 == star2)continue;
-        p[star2] = star1;
+        p[star1] = star2;
         answer += edge[i].first;
-        cnt++;
-        if(cnt == n-1)break;
     }
     cout << answer;
     return 0;
